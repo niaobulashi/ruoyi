58f0157b91a6e2c082f4d9591610e23c72c48ad3
hulang <hulang6666@qq.com>
2020-12-11 13:37:13 +0800

    增加日志记录过滤对象类型（解决多文件场景报错）

--- ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/LogAspect.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/LogAspect.java
@@ -1,6 +1,8 @@
 package com.ruoyi.framework.aspectj;
 
 import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,7 +34,7 @@ import com.ruoyi.system.domain.SysOperLog;
 
 /**
  * 操作日志记录处理
- * 
+ *
  * @author ruoyi
  */
 @Aspect
@@ -40,13 +42,13 @@ import com.ruoyi.system.domain.SysOperLog;
 public class LogAspect
 {
     private static final Logger log = LoggerFactory.getLogger(LogAspect.class);
-
+    
     // 配置织入点
     @Pointcut("@annotation(com.ruoyi.common.annotation.Log)")
     public void logPointCut()
     {
     }
-
+    
     /**
      * 处理完请求后执行
      *
@@ -57,10 +59,10 @@ public class LogAspect
     {
         handleLog(joinPoint, null, jsonResult);
     }
-
+    
     /**
      * 拦截异常操作
-     * 
+     *
      * @param joinPoint 切点
      * @param e 异常
      */
@@ -69,7 +71,7 @@ public class LogAspect
     {
         handleLog(joinPoint, e, null);
     }
-
+    
     protected void handleLog(final JoinPoint joinPoint, final Exception e, Object jsonResult)
     {
         try
@@ -80,10 +82,10 @@ public class LogAspect
             {
                 return;
             }
-
+            
             // 获取当前的用户
             LoginUser loginUser = SpringUtils.getBean(TokenService.class).getLoginUser(ServletUtils.getRequest());
-
+            
             // *========数据库日志=========*//
             SysOperLog operLog = new SysOperLog();
             operLog.setStatus(BusinessStatus.SUCCESS.ordinal());
@@ -92,13 +94,13 @@ public class LogAspect
             operLog.setOperIp(ip);
             // 返回参数
             operLog.setJsonResult(JSON.toJSONString(jsonResult));
-
+            
             operLog.setOperUrl(ServletUtils.getRequest().getRequestURI());
             if (loginUser != null)
             {
                 operLog.setOperName(loginUser.getUsername());
             }
-
+            
             if (e != null)
             {
                 operLog.setStatus(BusinessStatus.FAIL.ordinal());
@@ -123,10 +125,10 @@ public class LogAspect
             exp.printStackTrace();
         }
     }
-
+    
     /**
      * 获取注解中对方法的描述信息 用于Controller层注解
-     * 
+     *
      * @param log 日志
      * @param operLog 操作日志
      * @throws Exception
@@ -146,10 +148,10 @@ public class LogAspect
             setRequestValue(joinPoint, operLog);
         }
     }
-
+    
     /**
      * 获取请求的参数，放到log中
-     * 
+     *
      * @param operLog 操作日志
      * @throws Exception 异常
      */
@@ -167,7 +169,7 @@ public class LogAspect
             operLog.setOperParam(StringUtils.substring(paramsMap.toString(), 0, 2000));
         }
     }
-
+    
     /**
      * 是否存在注解，如果存在就获取
      */
@@ -176,14 +178,14 @@ public class LogAspect
         Signature signature = joinPoint.getSignature();
         MethodSignature methodSignature = (MethodSignature) signature;
         Method method = methodSignature.getMethod();
-
+        
         if (method != null)
         {
             return method.getAnnotation(Log.class);
         }
         return null;
     }
-
+    
     /**
      * 参数拼装
      */
@@ -203,15 +205,37 @@ public class LogAspect
         }
         return params.trim();
     }
-
+    
     /**
      * 判断是否需要过滤的对象。
-     * 
+     *
      * @param o 对象信息。
      * @return 如果是需要过滤的对象，则返回true；否则返回false。
      */
     public boolean isFilterObject(final Object o)
     {
+        Class<?> clazz = o.getClass();
+        if (clazz.isArray())
+        {
+            return clazz.getComponentType().isAssignableFrom(MultipartFile.class);
+        }
+        else if (Collection.class.isAssignableFrom(clazz))
+        {
+            Collection collection = (Collection) o;
+            for (Iterator iter = collection.iterator(); iter.hasNext();)
+            {
+                return iter.next() instanceof MultipartFile;
+            }
+        }
+        else if (Map.class.isAssignableFrom(clazz))
+        {
+            Map map = (Map) o;
+            for (Iterator iter = map.entrySet().iterator(); iter.hasNext(); )
+            {
+                Map.Entry entry = (Map.Entry) iter.next();
+                return entry.getValue() instanceof MultipartFile;
+            }
+        }
         return o instanceof MultipartFile || o instanceof HttpServletRequest || o instanceof HttpServletResponse;
     }
 }
