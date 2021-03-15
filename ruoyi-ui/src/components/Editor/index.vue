8c2f0971c5c000a64f85165bac1f794a1d4dda2d
hulang <hulang6666@qq.com>
2021-03-15 14:11:13 +0800

    富文本编辑组件支持只读

--- ruoyi-ui/src/components/Editor/index.vue
+++ ruoyi-ui/src/components/Editor/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="editor" ref="editor" :style="styles"></div>
+  <div class="editor" ref="editor" :style="styles"></div>
 </template>
 
 <script>
@@ -26,6 +26,11 @@ export default {
       type: Number,
       default: null,
     },
+    /* 只读 */
+    readOnly: {
+      type: Boolean,
+      default: false,
+    }
   },
   data() {
     return {
@@ -41,19 +46,17 @@ export default {
             ["bold", "italic", "underline", "strike"],       // 加粗 斜体 下划线 删除线
             ["blockquote", "code-block"],                    // 引用  代码块
             [{ list: "ordered" }, { list: "bullet" }],       // 有序、无序列表
-            [{ 'script': 'sub' }, { 'script': 'super' }],    // 右下标、右上标
             [{ indent: "-1" }, { indent: "+1" }],            // 缩进
             [{ size: ["small", false, "large", "huge"] }],   // 字体大小
             [{ header: [1, 2, 3, 4, 5, 6, false] }],         // 标题
             [{ color: [] }, { background: [] }],             // 字体颜色、字体背景颜色
-            [{ font: [] }],                                  // 字体种类
             [{ align: [] }],                                 // 对齐方式
             ["clean"],                                       // 清除文本格式
             ["link", "image", "video"]                       // 链接、图片、视频
           ],
         },
         placeholder: "请输入内容",
-        readOnly: false,
+        readOnly: this.readOnly,
       },
     };
   },
