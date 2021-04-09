29efe01f3cf56fa7fdcd93d3519c43f1e1e0f5c7
hulang <hulang6666@qq.com>
2021-04-09 16:14:23 +0800

    RepeatedlyRequestWrapper.ServletInputStream 实现available方法

--- ruoyi-common/src/main/java/com/ruoyi/common/filter/RepeatedlyRequestWrapper.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/filter/RepeatedlyRequestWrapper.java
@@ -13,59 +13,64 @@ import com.ruoyi.common.utils.http.HttpHelper;
 
 /**
  * 构建可重复读取inputStream的request
- * 
+ *
  * @author ruoyi
  */
 public class RepeatedlyRequestWrapper extends HttpServletRequestWrapper
 {
     private final byte[] body;
-
+    
     public RepeatedlyRequestWrapper(HttpServletRequest request, ServletResponse response) throws IOException
     {
         super(request);
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-
+        
         body = HttpHelper.getBodyString(request).getBytes("UTF-8");
     }
-
+    
     @Override
     public BufferedReader getReader() throws IOException
     {
         return new BufferedReader(new InputStreamReader(getInputStream()));
     }
-
+    
     @Override
     public ServletInputStream getInputStream() throws IOException
     {
-
+        
         final ByteArrayInputStream bais = new ByteArrayInputStream(body);
-
+        
         return new ServletInputStream()
         {
-
+            
             @Override
             public int read() throws IOException
             {
                 return bais.read();
             }
-
+            
             @Override
             public boolean isFinished()
             {
                 return false;
             }
-
+            
             @Override
             public boolean isReady()
             {
                 return false;
             }
-
+            
             @Override
             public void setReadListener(ReadListener readListener)
             {
-
+            
+            }
+            
+            @Override
+            public int available() throws IOException {
+                return body.length;
             }
         };
     }
