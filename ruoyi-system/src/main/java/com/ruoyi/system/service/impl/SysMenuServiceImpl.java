2be6ac66c8770bf6cf7a3433f384b0b728b5a8e7
hulang <hulang6666@qq.com>
2021-04-09 16:18:51 +0800

    优化

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysMenuServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysMenuServiceImpl.java
@@ -26,26 +26,26 @@ import com.ruoyi.system.service.ISysMenuService;
 
 /**
  * 菜单 业务层处理
- * 
+ *
  * @author ruoyi
  */
 @Service
 public class SysMenuServiceImpl implements ISysMenuService
 {
     public static final String PREMISSION_STRING = "perms[\"{0}\"]";
-
+    
     @Autowired
     private SysMenuMapper menuMapper;
-
+    
     @Autowired
     private SysRoleMapper roleMapper;
-
+    
     @Autowired
     private SysRoleMenuMapper roleMenuMapper;
-
+    
     /**
      * 根据用户查询系统菜单列表
-     * 
+     *
      * @param userId 用户ID
      * @return 菜单列表
      */
@@ -54,10 +54,10 @@ public class SysMenuServiceImpl implements ISysMenuService
     {
         return selectMenuList(new SysMenu(), userId);
     }
-
+    
     /**
      * 查询系统菜单列表
-     * 
+     *
      * @param menu 菜单信息
      * @return 菜单列表
      */
@@ -77,10 +77,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         }
         return menuList;
     }
-
+    
     /**
      * 根据用户ID查询权限
-     * 
+     *
      * @param userId 用户ID
      * @return 权限列表
      */
@@ -98,10 +98,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         }
         return permsSet;
     }
-
+    
     /**
      * 根据用户ID查询菜单
-     * 
+     *
      * @param userId 用户名称
      * @return 菜单列表
      */
@@ -119,10 +119,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         }
         return getChildPerms(menus, 0);
     }
-
+    
     /**
      * 根据角色ID查询菜单树信息
-     * 
+     *
      * @param roleId 角色ID
      * @return 选中菜单列表
      */
@@ -132,10 +132,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         SysRole role = roleMapper.selectRoleById(roleId);
         return menuMapper.selectMenuListByRoleId(roleId, role.isMenuCheckStrictly());
     }
-
+    
     /**
      * 构建前端路由所需要的菜单
-     * 
+     *
      * @param menus 菜单列表
      * @return 路由列表
      */
@@ -158,7 +158,7 @@ public class SysMenuServiceImpl implements ISysMenuService
                 router.setRedirect("noRedirect");
                 router.setChildren(buildMenus(cMenus));
             }
-            else if (isMeunFrame(menu))
+            else if (isMenuFrame(menu))
             {
                 List<RouterVo> childrenList = new ArrayList<RouterVo>();
                 RouterVo children = new RouterVo();
@@ -173,10 +173,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         }
         return routers;
     }
-
+    
     /**
      * 构建前端所需要树结构
-     * 
+     *
      * @param menus 菜单列表
      * @return 树结构列表
      */
@@ -205,10 +205,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         }
         return returnList;
     }
-
+    
     /**
      * 构建前端所需要下拉树结构
-     * 
+     *
      * @param menus 菜单列表
      * @return 下拉树结构列表
      */
@@ -218,10 +218,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         List<SysMenu> menuTrees = buildMenuTree(menus);
         return menuTrees.stream().map(TreeSelect::new).collect(Collectors.toList());
     }
-
+    
     /**
      * 根据菜单ID查询信息
-     * 
+     *
      * @param menuId 菜单ID
      * @return 菜单信息
      */
@@ -230,10 +230,10 @@ public class SysMenuServiceImpl implements ISysMenuService
     {
         return menuMapper.selectMenuById(menuId);
     }
-
+    
     /**
      * 是否存在菜单子节点
-     * 
+     *
      * @param menuId 菜单ID
      * @return 结果
      */
@@ -243,10 +243,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         int result = menuMapper.hasChildByMenuId(menuId);
         return result > 0 ? true : false;
     }
-
+    
     /**
      * 查询菜单使用数量
-     * 
+     *
      * @param menuId 菜单ID
      * @return 结果
      */
@@ -256,10 +256,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         int result = roleMenuMapper.checkMenuExistRole(menuId);
         return result > 0 ? true : false;
     }
-
+    
     /**
      * 新增保存菜单信息
-     * 
+     *
      * @param menu 菜单信息
      * @return 结果
      */
@@ -268,10 +268,10 @@ public class SysMenuServiceImpl implements ISysMenuService
     {
         return menuMapper.insertMenu(menu);
     }
-
+    
     /**
      * 修改保存菜单信息
-     * 
+     *
      * @param menu 菜单信息
      * @return 结果
      */
@@ -280,10 +280,10 @@ public class SysMenuServiceImpl implements ISysMenuService
     {
         return menuMapper.updateMenu(menu);
     }
-
+    
     /**
      * 删除菜单管理信息
-     * 
+     *
      * @param menuId 菜单ID
      * @return 结果
      */
@@ -292,10 +292,10 @@ public class SysMenuServiceImpl implements ISysMenuService
     {
         return menuMapper.deleteMenuById(menuId);
     }
-
+    
     /**
      * 校验菜单名称是否唯一
-     * 
+     *
      * @param menu 菜单信息
      * @return 结果
      */
@@ -310,10 +310,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         }
         return UserConstants.UNIQUE;
     }
-
+    
     /**
      * 获取路由名称
-     * 
+     *
      * @param menu 菜单信息
      * @return 路由名称
      */
@@ -321,16 +321,16 @@ public class SysMenuServiceImpl implements ISysMenuService
     {
         String routerName = StringUtils.capitalize(menu.getPath());
         // 非外链并且是一级目录（类型为目录）
-        if (isMeunFrame(menu))
+        if (isMenuFrame(menu))
         {
             routerName = StringUtils.EMPTY;
         }
         return routerName;
     }
-
+    
     /**
      * 获取路由地址
-     * 
+     *
      * @param menu 菜单信息
      * @return 路由地址
      */
@@ -344,23 +344,23 @@ public class SysMenuServiceImpl implements ISysMenuService
             routerPath = "/" + menu.getPath();
         }
         // 非外链并且是一级目录（类型为菜单）
-        else if (isMeunFrame(menu))
+        else if (isMenuFrame(menu))
         {
             routerPath = "/";
         }
         return routerPath;
     }
-
+    
     /**
      * 获取组件信息
-     * 
+     *
      * @param menu 菜单信息
      * @return 组件信息
      */
     public String getComponent(SysMenu menu)
     {
         String component = UserConstants.LAYOUT;
-        if (StringUtils.isNotEmpty(menu.getComponent()) && !isMeunFrame(menu))
+        if (StringUtils.isNotEmpty(menu.getComponent()) && !isMenuFrame(menu))
         {
             component = menu.getComponent();
         }
@@ -370,22 +370,22 @@ public class SysMenuServiceImpl implements ISysMenuService
         }
         return component;
     }
-
+    
     /**
      * 是否为菜单内部跳转
-     * 
+     *
      * @param menu 菜单信息
      * @return 结果
      */
-    public boolean isMeunFrame(SysMenu menu)
+    public boolean isMenuFrame(SysMenu menu)
     {
         return menu.getParentId().intValue() == 0 && UserConstants.TYPE_MENU.equals(menu.getMenuType())
                 && menu.getIsFrame().equals(UserConstants.NO_FRAME);
     }
-
+    
     /**
      * 是否为parent_view组件
-     * 
+     *
      * @param menu 菜单信息
      * @return 结果
      */
@@ -393,10 +393,10 @@ public class SysMenuServiceImpl implements ISysMenuService
     {
         return menu.getParentId().intValue() != 0 && UserConstants.TYPE_DIR.equals(menu.getMenuType());
     }
-
+    
     /**
      * 根据父节点的ID获取所有子节点
-     * 
+     *
      * @param list 分类表
      * @param parentId 传入的父节点ID
      * @return String
@@ -416,10 +416,10 @@ public class SysMenuServiceImpl implements ISysMenuService
         }
         return returnList;
     }
-
+    
     /**
      * 递归列表
-     * 
+     *
      * @param list
      * @param t
      */
@@ -436,7 +436,7 @@ public class SysMenuServiceImpl implements ISysMenuService
             }
         }
     }
-
+    
     /**
      * 得到子节点列表
      */
@@ -454,7 +454,7 @@ public class SysMenuServiceImpl implements ISysMenuService
         }
         return tlist;
     }
-
+    
     /**
      * 判断是否有子节点
      */
