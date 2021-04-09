b1a413487f7c5579da08465d819a64aead05c4e8
hulang <hulang6666@qq.com>
2021-04-09 16:10:07 +0800

    通用Controller添加响应返回消息

--- ruoyi-common/src/main/java/com/ruoyi/common/core/controller/BaseController.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/controller/BaseController.java
@@ -20,13 +20,13 @@ import com.ruoyi.common.utils.sql.SqlUtil;
 
 /**
  * web层通用数据处理
- * 
+ *
  * @author ruoyi
  */
 public class BaseController
 {
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
-
+    
     /**
      * 将前台传递过来的日期格式的字符串，自动转化为Date类型
      */
@@ -43,7 +43,7 @@ public class BaseController
             }
         });
     }
-
+    
     /**
      * 设置请求分页数据
      */
@@ -58,7 +58,7 @@ public class BaseController
             PageHelper.startPage(pageNum, pageSize, orderBy);
         }
     }
-
+    
     /**
      * 响应请求分页数据
      */
@@ -72,10 +72,10 @@ public class BaseController
         rspData.setTotal(new PageInfo(list).getTotal());
         return rspData;
     }
-
+    
     /**
      * 响应返回结果
-     * 
+     *
      * @param rows 影响行数
      * @return 操作结果
      */
@@ -83,7 +83,50 @@ public class BaseController
     {
         return rows > 0 ? AjaxResult.success() : AjaxResult.error();
     }
-
+    
+    /**
+     * 响应返回结果
+     *
+     * @param result 结果
+     * @return 操作结果
+     */
+    protected AjaxResult toAjax(boolean result)
+    {
+        return result ? success() : error();
+    }
+    
+    /**
+     * 返回成功
+     */
+    public AjaxResult success()
+    {
+        return AjaxResult.success();
+    }
+    
+    /**
+     * 返回失败消息
+     */
+    public AjaxResult error()
+    {
+        return AjaxResult.error();
+    }
+    
+    /**
+     * 返回成功消息
+     */
+    public AjaxResult success(String message)
+    {
+        return AjaxResult.success(message);
+    }
+    
+    /**
+     * 返回失败消息
+     */
+    public AjaxResult error(String message)
+    {
+        return AjaxResult.error(message);
+    }
+    
     /**
      * 页面跳转
      */
